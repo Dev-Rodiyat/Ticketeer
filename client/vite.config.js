4733738deacc48131581a322bd07a1359b283759
Adedoyin13 <olajideadedoyinrodiyat@gmail.com>
2025-06-28 14:22:02 +0100

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,9 +6,12 @@ export default defineConfig({
   plugins: [react()],
   base: '/',
   define: {
-    __DEFINES__: JSON.stringify({}),
+    __DEFINES__: '{}',
+    __HMR_CONFIG_NAME__: '"default"',
+    __HMR_TIMEOUT__: '30000',
+    __HMR_ENABLE_OVERLAY__: 'true',
     global: 'globalThis',
-    'process.env': {},
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
   },
   build: {
     target: 'esnext',
