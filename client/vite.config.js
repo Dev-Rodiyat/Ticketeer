0defe53cab055098ed2e2c479b0b03e3fddd24d3
Adedoyin13 <olajideadedoyinrodiyat@gmail.com>
2025-06-28 14:00:40 +0100

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This is correct for Vercel
+  base: '/',
+  define: {
+    __DEFINES__: JSON.stringify({}),
+    global: 'globalThis',
+    'process.env': {},
+  },
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+  },
 })
