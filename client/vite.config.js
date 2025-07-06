8294d8284bf8bdcc37aefd0a70892861aa907c4a
Dev-Rodiyat <olajideadedoyinrodiyat@gmail.com>
2025-07-06 22:02:03 +0100

    Update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,24 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript',
-    },
-    strictPort: true,
-    port: 5173
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  }
-});
+  plugins: [react()]
+}); 
